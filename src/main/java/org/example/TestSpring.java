c86ea1faba7907563131d4d4f7ecd0ebca6bba75
bogdanmuh <bogdanmuhutdinov97@gmail.com>
2021-07-11 21:53:31 +0300

    считывавание с файоа данных  предеача констант

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -11,7 +11,7 @@ public class TestSpring {
          MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
          musicPlayer.playMusic();
         context.close();
-
-
+        System.out.println(musicPlayer.getName());
+         System.out.println(musicPlayer.getVolume());
      }
 }
