ce1e602ad4aab735f107fea21f6c66e51a016761
bogdanmuh <bogdanmuhutdinov97@gmail.com>
2021-07-11 17:54:37 +0300

    внедрение зависимостей без участи класс

--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,13 +7,11 @@ public class TestSpring {
          ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext(
                  "applicationContext.xml"
          );
-         //ClassPathXmlApplicationContext обращаеться к файлу applicationContext и считывает все bean-ы
-         Music music=context.getBean("MusicBean",Music.class);
-         //testBean - в applicationContext bean и  и вторым аргументом говорим что это класс
-         MusicPlayer musicPlayer = new MusicPlayer(music);
 
+         MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
          musicPlayer.playMusic();
-         context.close();
+        context.close();
 
-    }
+
+     }
 }
