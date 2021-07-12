aebac7898d402d7b13f98f24e26764fbb3f766a0
bogdanmuh <bogdanmuhutdinov97@gmail.com>
2021-07-12 19:45:07 +0300

    singelton, property , init.....

--- src/main/java/org/example/ClassicalMusic.java
+++ src/main/java/org/example/ClassicalMusic.java
@@ -1,6 +1,17 @@
 package org.example;
 
 public class ClassicalMusic implements Music {
+    private  ClassicalMusic(){ }
+    public static ClassicalMusic getClassicalMusic(){
+        return new ClassicalMusic();
+    }
+    // если будет не static то  невозможно будет создать обьект т.к. коструктор private
+    public void doMyInit(){
+        System.out.println("Doing my initialization");
+    }
+    public void doMyDestroy(){
+        System.out.println("Doing my Destruction");
+    }
     @Override
     public String getSong(){
         return "HUngarian Rhapsody";
--- src/main/java/org/example/TestSpring.java
+++ src/main/java/org/example/TestSpring.java
@@ -7,11 +7,18 @@ public class TestSpring {
          ClassPathXmlApplicationContext context= new ClassPathXmlApplicationContext(
                  "applicationContext.xml"
          );
+         ClassicalMusic classicalMusic = context.getBean("MusicBean",ClassicalMusic.class);
+         System.out.println(classicalMusic.getSong());
 
-         MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+         /*MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
          musicPlayer.playMusic();
-        context.close();
         System.out.println(musicPlayer.getName());
-         System.out.println(musicPlayer.getVolume());
+         System.out.println(musicPlayer.getVolume());*/
+         context.close();
+
      }
 }
+// создание бинов по умолчанию  используется scope singelton
+// т.е.MusicPlayer musicPlayer = context.getBean("musicPlayer",MusicPlayer.class);
+//  это строчка кода будет всегда возвращать ссылку на один тот же bean
+//scope  prototype для  вызовв функции getBean сздаеться свой объект
